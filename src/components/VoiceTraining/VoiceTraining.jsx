cf385d80213bbce9cb71c2a6c36bf3dad4f2ae9b
Jana Sandeyouni <Jnoo66@icloud.com>
2025-11-03 15:49:01 +0300

    add voiceTraining component

---
+++ src/components/VoiceTraining/VoiceTraining.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react"
+import { trainingAPI } from "../api/trainingAPI"
+
+export default function VoiceTraining() {
+  
+
+  return (
+    <div>
+      <h1>Describe the Word</h1>
+
+    </div>
+  )
+}
