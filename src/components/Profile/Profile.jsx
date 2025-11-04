34ddd78d96f2761c51d178a556b73e2a5c0d6d44
Jana Sandeyouni <Jnoo66@icloud.com>
2025-11-04 16:08:45 +0300

    add getProfileData to fetch data from user and profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,48 @@
 import React, { useState, useEffect } from "react"
-import { authRequest, getUserFromToken } from "../../lib/auth"
+import { authRequest } from "../../lib/auth"
 import axios from 'axios'
 
 export default function Profile({user}) {
 
-  const [profile, setProfile] = useState('')
-  const [userData, setUserData] = useState('')
+  const [profile, setProfile] = useState(null)
+  const [userData, setUserData] = useState(null)
   const [formData, setFormData] = useState({
     full_name: '',
     age: '',
-    owner: user
   })
 
-  async function getUserData() {
-        const response = await axios.get(`http://127.0.0.1:8000/api/users/current/`)
-        console.log(response.data)
-        setUserData(response.data)
-    }
+  //Source helper: https://stackoverflow.com/questions/50046841/proper-way-to-make-api-fetch-post-with-async-await
+  async function getProfileData() {
+  try {
+    // Get user info (id, username, email, first_name, last_name )
+    const userResponse = await authRequest({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/api/users/current/'
+    })
+    setUserData(userResponse.data)
+    console.log(userResponse.data)
 
-    useEffect(() => {
-        if (user) {
-            getUserData()
-        }
-    }, [])
+    // Get profile info (full_name, age, total_training_time, created_at)
+    const profileResponse = await authRequest({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/api/profile/'
+    })
+    setProfile(profileResponse.data)
+    console.log(profileResponse.data)
+    
+    setFormData({
+      full_name: profileResponse.data.full_name ,
+      age: profileResponse.data.age
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+useEffect(() => {
+  getProfileData()
+}, [])
+
 
   return (
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
