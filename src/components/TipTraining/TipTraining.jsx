c30b339007be827489cdc2dc4c429ed93312908f
Jana Sandeyouni <Jnoo66@icloud.com>
2025-11-04 13:29:02 +0300

    add loading for the tips

--- src/components/TipTraining/TipTraining.jsx
+++ src/components/TipTraining/TipTraining.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { authRequest } from "../../lib/auth"
 import axios from 'axios'
 
+// source helper: https://git.generalassemb.ly/SDA-SEB-02-V/DRF-example/blob/main/cat-collector-frontend/src/components/CatDetail/CatDetail.jsx
 export default function TipTraining() {
 
   const [tip, setTip] = useState(null)
@@ -29,12 +30,16 @@ export default function TipTraining() {
   return (
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
       <h1>Tips & Motivation</h1>
+      { tip ?(
       <div>
         <div>{tip.category}</div>
         <h2>{tip.title}</h2>
         <p>{tip.content}</p>
         <p>— {tip.author}</p>
       </div>
+      ) :(
+        <p>Loading...</p>
+      )}
       <button onClick={getRandomTip}>Get Another Tip</button>
     </div>
   )
