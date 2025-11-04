42d6e822190593b6a9eff7c9c2578482fc92cf42
Jana Sandeyouni <Jnoo66@icloud.com>
2025-11-04 13:24:55 +0300

    add TipTraining component to fetch data

--- src/components/TipTraining/TipTraining.jsx
+++ src/components/TipTraining/TipTraining.jsx
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from "react"
 import { authRequest } from "../../lib/auth"
+import axios from 'axios'
 
 export default function TipTraining() {
 
+  const [tip, setTip] = useState(null)
+  const [errors, setErrors] = useState(null)
+
+  async function getRandomTip() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/tip/')
+      console.log(response.data)
+      setTip(response.data)
+    } catch (error) {
+      console.log(error)
+      setErrors(error.response.data.error)
+    }
+  }
+
+  useEffect(() => {
+    getRandomTip()
+  }, [])
+
+  if (errors) {
+    return <h3>{errors}</h3>
+  }
 
   return (
     <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-      <h1>Take some Tips!</h1>
-
+      <h1>Tips & Motivation</h1>
+      <div>
+        <div>{tip.category}</div>
+        <h2>{tip.title}</h2>
+        <p>{tip.content}</p>
+        <p>— {tip.author}</p>
+      </div>
+      <button onClick={getRandomTip}>Get Another Tip</button>
     </div>
   )
 }
